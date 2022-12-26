3e3e147901a871a5da456e52f7371b68a12ccc7b
GuiCoelhoDev <guiminervino@gmail.com>
2022-12-26 00:00:25 -0300

    continuing api

--- test_api.py
+++ test_api.py
@@ -1,9 +1,31 @@
 from fastapi import FastAPI
 
+from typing import Optional
+
+from pydantic import BaseModel
+
 app = FastAPI()
 
-@app.get("/")
-def index():
-    return {'data': {'name': 'Gui'}}
+class Disciplina(BaseModel):
+    nome: str
+    codigo: str
+    carga_horaria: int
 
+
+
+@app.get("/disciplinas")
+def index(limit=10, sort: Optional[str] = None):
+
+    if sort:
+        return {'data': f'lista de {limit} disciplinas ordenadas por {sort}'}
+    else:
+        return {'data': f'lista de {limit} disciplinas'}
+
+@app.get('/disciplinas/{id}')
+def show(id: int):
+    return {'data': id}
+
+@app.post('/disciplinas')
+def create(request: Disciplina):
+   return {'data': f"disciplina {request.nome} criada"}
 
