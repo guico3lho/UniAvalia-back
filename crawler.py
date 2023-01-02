10cc3b8771995fc10dd50d61b7c94d4c716230e9
GuiCoelhoDev <guiminervino@gmail.com>
2023-01-01 21:11:30 -0300

    Remove comentários

--- crawler.py
+++ crawler.py
@@ -1,17 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
-
-url = "https://sigaa.unb.br/sigaa/public/turmas/listar.jsf"
 from sqlalchemy.orm import Session
 from app import database, models
 import re
 
-query = """
-CREATE disciplina_professor_details as SELECT d.nome as Disciplina, p.nome as Professor
-FROM disciplina_professor dp
-INNER JOIN disciplina d ON d.id = dp.disciplina_id
-INNER JOIN professor p ON p.id = dp.professor_id
-"""
+url = "https://sigaa.unb.br/sigaa/public/turmas/listar.jsf"
 
 
 def main():
@@ -24,42 +17,8 @@ def main():
         database.create_db()
 
     session = database.SessionLocal()
-    # parse_oferta(508, 2022, 2, session)
+    parse_oferta(508, 2022, 2, session)
 
-    create_disciplina_professor_details(session)
-
-def create_disciplina_professor_details(session: Session):
-    ...
-    # session.query(models.disciplina_professor).join(models.Disciplina).join(models.Professor).with_entities(models.disciplina_professor, models.Disciplina.nome, models.Professor.nome).all()
-    session.query(models.disciplina_professor).join(models.Disciplina, models.Disciplina.id).join(models.Professor, models.Professor.id)
-
-
-# def create_disciplina_professor_details(session):
-#     metadata = MetaData(bind=session.bind)
-#     models.DisciplinaProfessorDetails.__table__.create(bind=database.engine)
-#     dp = Table('disciplina_professor', metadata, autoload=True)
-#     disciplina = Table('disciplina', metadata, autoload=True)
-#     professor = Table('professor', metadata, autoload=True)
-#
-#     mapper(models.DisciplinaProfessorDetails, 'disciplina_professor_details', properties={
-#         'disciplina': [disciplina.c.nome],
-#         'professor': [professor.c.nome]
-#     })
-#
-#
-#     result = session.query(models.DisciplinaProfessorDetails).join(disciplina,
-#                                                             disciplina.c.id == dp.c.disciplina_id).join(
-#         professor, professor.c.id == dp.c.professor_id).all()
-#
-#
-# de
-
-
-
-
-# Para cada matéria, criar objeto do tipo disciplina
-# para cada professor da matéria, criar objeto do tipo professor
-# conectar disciplina com professor
 
 def parse_oferta(dep_id, ano, periodo, session: Session):
     request_data = get_request_from_oferta()
@@ -77,7 +36,7 @@ def parse_oferta(dep_id, ano, periodo, session: Session):
         'Upgrade-Insecure-Requests': '1',
         'Cache-Control': 'max-age=0'
     }
-    disciplinas_list = []
+
     response = requests.request("POST", url, headers=headers, data=payload)
     html_soup = BeautifulSoup(response.text.encode('utf8'), 'html.parser')
 
@@ -87,21 +46,16 @@ def parse_oferta(dep_id, ano, periodo, session: Session):
     indice_disciplina_atual = 0
 
     disciplina = None
-    professor = None
-    flag_nova_disciplina_encontrada = 0
+
     for i, disciplina_professor in enumerate(disciplinas_professores):
 
         type = disciplinas_professores[i].attrs['class'][0]
         if type == 'agrupador':
-            # if flag_nova_disciplina_encontrada == 1:
-            #     session.add(disciplina)
-            #     session.commit()
 
-            flag_nova_disciplina_encontrada = 1
+
             indice_disciplina_atual += 1
             print(f'Parsing disciplina {indice_disciplina_atual}/{len_disciplinas}')
             codigo_e_disciplina = disciplina_professor.td.a.span.text.split(' - ')
-            # disciplina = {'codigo': codigo_e_disciplina[0], 'nome': codigo_e_disciplina[1]}
             disciplina = models.Disciplina(codigo=codigo_e_disciplina[0], nome=codigo_e_disciplina[1])
             continue
 
@@ -124,42 +78,12 @@ def parse_oferta(dep_id, ano, periodo, session: Session):
             print("Não é professor nem disciplina")
     session.close()
 
-    # create disciplina_professor table with disciplina and professor names
-
-#
-
-
-
-
 
 def get_request_from_oferta():
     response = requests.request("GET", url)
     html_soup = BeautifulSoup(response.text.encode('utf8'), 'html.parser')
     return {"cookies": response.headers["Set-Cookie"].split(' ')[0],
             "javax": html_soup.select('#javax\.faces\.ViewState')[0]['value']}
-    # disciplinas_span = html_soup.find_all('span', class_='tituloDisciplina')
-    # for disciplina_parent in disciplinas_span:
-    #     codigo_disciplina = disciplina_parent.text
-    #     codigo, nome = codigo_disciplina.split(' - ')
-    #     disciplinas_list.append({'codigo': codigo, 'nome': nome})
-    #
-    # return disciplinas_list
-
-    # url_api = 'http://127.0.0.1:8000/disciplinas/'
-    # response = requests.post(url_api, json=disciplinas_list)
-    # if response.status_code == 200:
-    #     print(response.json())
-    # else:
-    #     print(response.status_code)
-
-
-
-
-
-# def send_sqlalchemy_sqlite(disciplinas_list, s: Session):
-#     data = [models.Disciplina(**d) for d in disciplinas_list]
-#     s.add_all(data)
-#     s.commit()
 
 
 if __name__ == '__main__':
